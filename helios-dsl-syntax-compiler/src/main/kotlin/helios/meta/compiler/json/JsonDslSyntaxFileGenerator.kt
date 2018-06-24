46e169c58baadd1fd5ba999109e3399aa4e8a7a5
Simon Vergauwen <vergauwen.simon@gmail.com>
2018-06-24 22:55:24 +0200

    Add KDoc to generated code

--- helios-dsl-syntax-compiler/src/main/kotlin/helios/meta/compiler/json/JsonDslSyntaxFileGenerator.kt
+++ helios-dsl-syntax-compiler/src/main/kotlin/helios/meta/compiler/json/JsonDslSyntaxFileGenerator.kt
@@ -24,24 +24,25 @@ data class JsonElement(
             inline get() = target.classOrPackageProto.nameResolver
 }
 
-private val JsonElement.jsclass: JsonClass? get()=
-  (target.classOrPackageProto as? ClassOrPackageDataWrapper.Class)?.run {
-    JsonClass(fullName.replace("/","."), simpleName)
-  }
+private val JsonElement.typeName: String? get()=
+  (target.classOrPackageProto as? ClassOrPackageDataWrapper.Class)?.simpleName
 
-private data class JsonClass(val fqName: String, val name: String)
-private data class DslContent(val keys: List<Name>,val types: List<JsonClass>)
+private data class DslContent(val keys: List<Name>,val typeNames: List<String>)
 
 class JsonDslSyntaxFileGenerator(
         private val generatedDir: File,
         jsonAnnotatedList: List<JsonAnnotated>
 ) {
 
-    private val Json = "helios.core.Json"
+    private val Json = "Json"
+    private val Optional = "Optional"
+    private val Travesal = "Travesal"
     private val imports = """
+      |import arrow.optics.Optional
+      |import arrow.optics.Traversal
+      |import helios.core.Json
       |import helios.optics.select
       |import helios.optics.extract
-      |
       |""".trimMargin()
 
     private val packageSyntax: List<Pair<Package, DslContent>> = jsonAnnotatedList
@@ -49,38 +50,51 @@ class JsonDslSyntaxFileGenerator(
             .groupBy(JsonElement::`package`)
             .mapValues { (_, v) ->
               DslContent(v.flatMap { element -> element.properties.map { element.nameResolver.getName(it.name) } }.distinct(),
-                v.mapNotNull { it.jsclass })
+                v.mapNotNull { it.typeName })
             }.toList()
     /**
      * Main entry point for json dsl syntax generation
      */
-    fun generate() = packageSyntax.forEach { (`package`, content) ->
-      val (keys, types) = content
-        val keyDsl = keys.joinToString(prefix = "package $`package`\n\n$imports", separator = "\n") {
+    fun generate() = packageSyntax.forEach { (`package`, data) ->
+      val keys = data.keys.joinToString(separator = "\n") {
           """
-            |inline val $Optional<$Json, $Json>.$it: $Optional<$Json, $Json>
-            |    inline get() = select("$it")
-            |
-            |inline val $Traversal<$Json, $Json>.$it: $Traversal<$Json, $Json>
-            |    inline get() = select("$it")
-            |""".trimMargin()
-        }
-
-        val typeDsl = types.joinToString(separator = "\n") { (fqName, name) ->
-          """
-          |fun $Optional<$Json, $Json>.to$name(): $Optional<$Json, $fqName>
-          |    = extract($fqName.decoder(), $fqName.encoder())
+          |/**
+          |  * Select $it key in [JsObject].
+          |  */
+          |inline val $Optional<$Json, $Json>.$it: $Optional<$Json, $Json>
+          |    inline get() = select("$it")
           |
-          |fun $Traversal<$Json, $Json>.to$name(): $Traversal<$Json, $fqName>
-          |    = extract($fqName.decoder(), $fqName.encoder())
+          |/**
+          |  * Select $it key in [JsObject].
+          |  */
+          |inline val $Traversal<$Json, $Json>.$it: $Traversal<$Json, $Json>
+          |    inline get() = select("$it")
+          |""".trimMargin() }
+
+      val types = data.typeNames.joinToString(separator = "\n") { name ->
+          """
+          |/**
+          |  * Extract [$name] from [Json.Companion.path]
+          |  */
+          |fun $Optional<$Json, $Json>.to$name(): $Optional<$Json, $name>
+          |    = extract($name.decoder(), $name.encoder())
+          |
+          |/**
+          |  * Extract [$name] from [Json.Companion.path]
+          |  */
+          |fun $Traversal<$Json, $Json>.to$name(): $Traversal<$Json, $name>
+          |    = extract($name.decoder(), $name.encoder())
           |""".trimMargin()
         }
-        val file = File(generatedDir, "${jsonAnnotationClass.simpleName}.helios.dsl.syntax.$`package`.kt")
-        file.writeText("""
-          |$keyDsl
-          |
-          |$typeDsl
-        """.trimMargin())
+
+      val file = File(generatedDir, "${jsonAnnotationClass.simpleName}.helios.dsl.syntax.$`package`.kt")
+      file.writeText("""
+        |package $`package`
+        |
+        |$imports
+        |$keys
+        |$types
+      """.trimMargin())
     }
 
 }
