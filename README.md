d66d09ce035289cfe2bc800b8cdcc4c3f5c1a82e
Adrian Ramirez Fornell <15971742+AdrianRaFo@users.noreply.github.com>
2018-12-19 12:09:04 +0100

    Add readme and improve a little the sample (#31)
    * wip readme and improve a little the sample
    
    * add deps
    
    * indent json string
    
    * add jitpack
    
    * use safe api on sample and readme
    
    * use ank for quickstart
    
    * Delete println
    
    * Update README.md
    
    Co-Authored-By: AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
    
    * fix some parts of the quickstart
    
    * minor improvement to the readme
    
    * better intro and benchmarks
    
    * minor fix

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-EXPERIMENTAL WIP Json library based on a port of the [Jawn Parser](https://github.com/non/jawn) built on Arrow.
+# Helios
+
+**Helios** is a library used to transform `Json` text into a model and vice versa. 
+It's based on a part of the [Jawn Parser](https://github.com/non/jawn) built on `Arrow`, 
+a Functional companion to Kotlin's Standard Library.
+
+## Adding the dependency
+
+**Helios** uses kotlin `1.3.10` version and `Arrow` `0.8.1` version.
+
+To import the library on `Gradle` add the following repository and dependencies:
+
+```groovy
+repositories {
+      maven { url "https://jitpack.io" }
+ }
+ 
+dependencies {
+    compile "com.47deg:helios-core:0.0.1-SNAPSHOT"
+    compile "com.47deg:helios-meta:0.0.1-SNAPSHOT"
+    compile "com.47deg:helios-parser:0.0.1-SNAPSHOT"
+}
+```
+
+## QuickStart
+
+You can find a quick-start [here](https://github.com/47deg/helios/tree/master/helios-docs/docs/QuickStart.md).
+
+## Why Helios
+
+**Helios** is the one of the fastest `Json` parser library on `Kotlin` 
+with the advantage of the `Arrow` functional programming
+
+### Decoding
+Library | Score | Score Error (99.9%) | Unit |
+|--|--|--|--|
+helios | 19032.522733 | 1511.699343 | ops/s |
+jackson | 32247.369607 | 2506.006530 | ops/s |
+klaxon | 571.281780 | 14.468980 | ops/s |
+kotson | 29600.046472 | 632.968692  | ops/s |
+moshi | 33326.678828 | 1062.384473 | ops/s |
+
+### DecodingFromRaw
+Library | Score | Score Error (99.9%) | Unit |
+|--|--|--|--|
+helios | 9334.929365 | 7684.760121 | ops/s |
+jackson | 23789.397621 | 2854.291651 | ops/s |
+klaxon | 452.653645 | 79.123214 | ops/s |
+kotlinx | 38650.988483 | 2198.522152 | ops/s |
+kotson | 38081.187808 | 952.164481 | ops/s |
+moshi | 25571.429371 | 1157.867183 | ops/s |
+
+### Parsing
+Library | Score | Score Error (99.9%) | Unit |
+|--|--|--|--|
+helios | 29960.715221 | 2098.828841 | ops/s |
+jackson | 42058.986030 | 3536.245777 | ops/s |
+jsonIter | 33036.995781 | 515.394397 | ops/s |
+kotson | 29249.670413 | 2863.133433 | ops/s |
+moshi  | 19017.079668 | 1202.955223 | ops/s |
+     
+## Running Benchmarks
+
+To run the benchmarks execute the following command:
+
+`./gradlew :helios-benchmarks:jmh -PjmhInclude=helios.benchmarks.HeliosBenchmark`
--- helios-sample/src/main/kotlin/helios/sample/sample.kt
+++ helios-sample/src/main/kotlin/helios/sample/sample.kt
@@ -1,83 +1,98 @@
 package helios.sample
 
-import arrow.core.Either
+import arrow.core.*
 import helios.core.*
 import helios.optics.*
 import helios.typeclasses.DecodingError
 
-const val companyJsonString = """
-{
-  "name": "Arrow",
-  "address": {
-    "city": "Functional Town",
-    "street": {
-      "number": 1337,
-      "name": "Functional street"
-    }
-  },
-  "employees": [
+object Sample {
+  const val companyJsonString = """
     {
-      "name": "John",
-      "lastName": "doe"
-    },
-    {
-      "name": "Jane",
-      "lastName": "doe"
+      "name": "Arrow",
+      "address": {
+        "city": "Functional Town",
+        "street": {
+          "number": 1337,
+          "name": "Functional street"
+        }
+      },
+      "employees": [
+        {
+          "name": "John",
+          "lastName": "doe"
+        },
+        {
+          "name": "Jane",
+          "lastName": "doe"
+        }
+      ]
+    }"""
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+
+    val companyJson =
+      Json.parseFromString(companyJsonString).getOrHandle {
+        println("Failed creating the Json ${it.localizedMessage}, creating an empty one")
+        JsString("")
+      }
+
+    val errorOrCompany: Either<DecodingError, Company> = Company.decoder().decode(companyJson)
+
+    errorOrCompany.fold({
+      println("Something went wrong during decoding: $it")
+    }, {
+      println("Successfully decode the json: $it")
+    })
+
+    val street = Street(47, "Degrees")
+
+    val streetJson: Json = with(Street.encoder()) {
+      street.encode()
     }
-  ]
-}"""
 
-fun main(args: Array<String>) {
+    println(streetJson.toJsonString())
 
-  val companyJson: Json = Json.parseUnsafe(companyJsonString)
+    Json.path.select("name").string.modify(companyJson, String::toUpperCase).let(::println)
+    Json.path.name.string.modify(companyJson, String::toUpperCase).let(::println)
 
-  val errorOrCompany: Either<DecodingError, Company> = Company.decoder().decode(companyJson)
+    Json.path.select("address").select("street").select("name").string.getOption(companyJson)
+      .let(::println)
+    Json.path.address.street.name.string.getOption(companyJson).let(::println)
 
-  errorOrCompany.fold({
-    println("Something went wrong during decoding: $it")
-  }, {
-    println("Successfully decode the json: $it")
-  })
+    Json.path.select("employees").every.select("lastName").string
+    val employeeLastNames = Json.path.employees.every.lastName.string
 
-  Json.path.select("name").string.modify(companyJson, String::toUpperCase).let(::println)
-  Json.path.name.string.modify(companyJson, String::toUpperCase).let(::println)
+    employeeLastNames.modify(companyJson, String::capitalize).let {
+      employeeLastNames.getAll(it)
+    }.let(::println)
 
-  Json.path.select("address").select("street").select("name").string.getOption(companyJson)
-    .let(::println)
-  Json.path.address.street.name.string.getOption(companyJson).let(::println)
+    Json.path.employees.filterIndex { it == 0 }.name.string.getAll(companyJson).let(::println)
 
-  Json.path.select("employees").every.select("lastName").string
-  val employeeLastNames = Json.path.employees.every.lastName.string
+    Json.path.employees.every.filterKeys { it == "name" }.string.getAll(companyJson).let(::println)
 
-  employeeLastNames.modify(companyJson, String::capitalize).let {
-    employeeLastNames.getAll(it)
-  }.let(::println)
-
-  Json.path.employees.filterIndex { it == 0 }.name.string.getAll(companyJson).let(::println)
-
-  Json.path.employees.every.filterKeys { it == "name" }.string.getAll(companyJson).let(::println)
-
-  val json: Json = JsObject(
-    "first_name" to JsString("John"),
-    "last_name" to JsString("Doe"),
-    "age" to JsNumber(28),
-    "siblings" to JsArray(
-      listOf(
-        JsObject(
-          "first_name" to JsString("Elia"),
-          "age" to JsNumber(23)
-        ),
-        JsObject(
-          "first_name" to JsString("Robert"),
-          "age" to JsNumber(25)
+    val json: Json = JsObject(
+      "first_name" to JsString("John"),
+      "last_name" to JsString("Doe"),
+      "age" to JsNumber(28),
+      "siblings" to JsArray(
+        listOf(
+          JsObject(
+            "first_name" to JsString("Elia"),
+            "age" to JsNumber(23)
+          ),
+          JsObject(
+            "first_name" to JsString("Robert"),
+            "age" to JsNumber(25)
+          )
         )
       )
     )
-  )
 
-  Json.path.select("siblings")[1]
-    .toSibling()
-    .first_name
-    .set(json, "Robert Jr.")
+    Json.path.select("siblings")[1]
+      .toSibling()
+      .first_name
+      .set(json, "Robert Jr.")
 
+  }
 }
