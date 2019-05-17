6dd459b52dc92c255fa97abcb3c3359bd1e1beb6
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-05-18 00:28:32 +0200

    format

--- helios-core/src/main/kotlin/helios/typeclasses/Decoder.kt
+++ helios-core/src/main/kotlin/helios/typeclasses/Decoder.kt
@@ -8,8 +8,8 @@ sealed class DecodingError
 data class StringDecodingError(val value: Json) : DecodingError()
 data class BooleanDecodingError(val value: Json) : DecodingError()
 data class NumberDecodingError(val value: Json) : DecodingError()
-data class ArrayDecodingError(val value:Json) : DecodingError()
-data class ObjectDecodingError(val value:Json) : DecodingError()
+data class ArrayDecodingError(val value: Json) : DecodingError()
+data class ObjectDecodingError(val value: Json) : DecodingError()
 data class KeyNotFound(val name: String) : DecodingError()
 
 @higherkind
