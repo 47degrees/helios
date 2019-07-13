b33703f73d3e15649833c4bed427c3eccdffa252
Andrzej Ressel <jereksel@gmail.com>
2019-07-13 22:18:22 +0200

    Add tests for NonEmptyList

--- helios-core/src/test/kotlin/helios/instances/InstancesArrowTest.kt
+++ helios-core/src/test/kotlin/helios/instances/InstancesArrowTest.kt
@@ -1,29 +1,29 @@
 package helios.instances
 
-import arrow.core.Either
 import arrow.core.Right
 import arrow.data.NonEmptyList
 import arrow.test.UnitSpec
 import arrow.test.generators.nonEmptyList
-import helios.core.Json
+import helios.core.JsArray
 import helios.test.generators.alphaStr
 import io.kotlintest.assertions.arrow.either.beLeft
 import io.kotlintest.properties.Gen
-import io.kotlintest.properties.forAll
+import io.kotlintest.properties.assertAll
 import io.kotlintest.should
+import io.kotlintest.shouldBe
 
 class InstancesArrowTest : UnitSpec() {
 
     init {
 
         "NonEmptyList should be encode and decode successfully" {
-            forAll(Gen.nonEmptyList(Gen.alphaStr())) { list ->
-                NonEmptyList.decoder(String.decoder()).decode(NonEmptyList.encoder(String.encoder()).run { list.encode() }) == Right(list)
+            assertAll(Gen.nonEmptyList(Gen.alphaStr())) { list ->
+                NonEmptyList.decoder(String.decoder()).decode(NonEmptyList.encoder(String.encoder()).run { list.encode() }) shouldBe Right(list)
             }
         }
 
         "NonEmptyList should fail for empty lists" {
-            val empty = (Json.parseFromString("[]") as Either.Right).b
+            val empty = JsArray(emptyList())
             NonEmptyList.decoder(String.decoder()).decode(empty) should beLeft()
         }
 
