2fe01591da9ec4b68ba2b79081d15647692df426
Andrzej Ressel <jereksel@gmail.com>
2019-07-14 02:10:48 +0200

    Add support for NonEmptyList (#89)
    * Add support for NonEmptyList
    
    * Add tests for NonEmptyList
    
    * Add tests for NonEmptyList
    
    * Add tests for NonEmptyList
    
    Co-Authored-By: Adrian Ramirez Fornell <15971742+AdrianRaFo@users.noreply.github.com>
    
    * Add tests for NonEmptyList

---
+++ helios-core/src/test/kotlin/helios/instances/InstancesArrowTest.kt
@@ -0,0 +1,31 @@
+package helios.instances
+
+import arrow.data.NonEmptyList
+import arrow.test.UnitSpec
+import arrow.test.generators.nonEmptyList
+import helios.core.JsArray
+import helios.test.generators.alphaStr
+import io.kotlintest.assertions.arrow.either.beLeft
+import io.kotlintest.assertions.arrow.either.beRight
+import io.kotlintest.properties.Gen
+import io.kotlintest.properties.assertAll
+import io.kotlintest.should
+
+class InstancesArrowTest : UnitSpec() {
+
+    init {
+
+        "NonEmptyList should be encode and decode successfully" {
+            assertAll(Gen.nonEmptyList(Gen.alphaStr())) { list ->
+                NonEmptyList.decoder(String.decoder()).decode(NonEmptyList.encoder(String.encoder()).run { list.encode() }) should beRight(list)
+            }
+        }
+
+        "NonEmptyList should fail for empty lists" {
+            val empty = JsArray(emptyList())
+            NonEmptyList.decoder(String.decoder()).decode(empty) should beLeft()
+        }
+
+    }
+
+}
