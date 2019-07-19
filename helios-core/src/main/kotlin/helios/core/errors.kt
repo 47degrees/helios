cb517926c54c90812c1d8ecedc6583f4b001221c
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-07-19 16:36:29 +0200

    make the errors a sealed inheritance

--- helios-core/src/main/kotlin/helios/core/errors.kt
+++ helios-core/src/main/kotlin/helios/core/errors.kt
@@ -3,17 +3,17 @@ package helios.core
 import arrow.core.Option
 import java.time.format.DateTimeFormatter
 
-interface DecodingError
-data class JsStringDecodingError(val value: Json) : DecodingError
-data class JsBooleanDecodingError(val value: Json) : DecodingError
-data class JsNumberDecodingError(val value: Json) : DecodingError
-data class JsArrayDecodingError(val value: Json) : DecodingError
-data class JsObjectDecodingError(val value: Json) : DecodingError
-data class KeyNotFound(val name: String) : DecodingError
-data class EnumValueNotFound(val value: Json) : DecodingError
-data class DateDecodingError(val value: Json, val formatter: Option<DateTimeFormatter>) : DecodingError
+sealed class DecodingError
+data class JsStringDecodingError(val value: Json) : DecodingError()
+data class JsBooleanDecodingError(val value: Json) : DecodingError()
+data class JsNumberDecodingError(val value: Json) : DecodingError()
+data class JsArrayDecodingError(val value: Json) : DecodingError()
+data class JsObjectDecodingError(val value: Json) : DecodingError()
+data class KeyNotFound(val name: String) : DecodingError()
+data class EnumValueNotFound(val value: Json) : DecodingError()
+data class DateDecodingError(val value: Json, val formatter: Option<DateTimeFormatter>) : DecodingError()
 data class ExceptionOnDecoding(
   val value: Json,
   val msg: String,
   val exception: Throwable? = null
-) : Throwable(msg, exception), DecodingError
+) : DecodingError()
