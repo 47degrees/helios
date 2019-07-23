12dced402f50e2a707783b601378352590680e7b
Adrian Ramirez Fornell <15971742+AdrianRaFo@users.noreply.github.com>
2019-07-23 14:52:13 +0200

    Improve error naming and handling (#103)
    * improve error naming and handling
    
    * make the errors a sealed inheritance
    
    * Fix testClasses after merge
    
    * add syntax methods
    
    * nested errors adt
    
    * fix docs
    
    * revert nested inheritance and create type specific jsnumber errors

--- helios-core/src/main/kotlin/helios/core/errors.kt
+++ helios-core/src/main/kotlin/helios/core/errors.kt
@@ -1,10 +1,36 @@
 package helios.core
 
+import arrow.core.Option
+import java.time.format.DateTimeFormatter
+
 sealed class DecodingError
-data class StringDecodingError(val value: Json) : DecodingError()
-data class BooleanDecodingError(val value: Json) : DecodingError()
-data class NumberDecodingError(val value: Json) : DecodingError()
-data class ArrayDecodingError(val value: Json) : DecodingError()
-data class ObjectDecodingError(val value: Json) : DecodingError()
+
+sealed class JsNumberDecodingError : DecodingError() {
+  abstract val value: Json
+  
+  data class JsBigDecimalError(override val value: Json) : JsNumberDecodingError()
+  data class JsBigIntegerError(override val value: Json) : JsNumberDecodingError()
+  data class JsDoubleError(override val value: Json) : JsNumberDecodingError()
+  data class JsFloatError(override val value: Json) : JsNumberDecodingError()
+  data class JsLongError(override val value: Json) : JsNumberDecodingError()
+  data class JsIntError(override val value: Json) : JsNumberDecodingError()
+  data class JsShortError(override val value: Json) : JsNumberDecodingError()
+  data class JsByteError(override val value: Json) : JsNumberDecodingError()
+
+}
+
 data class KeyNotFound(val name: String) : DecodingError()
+
+data class JsNullDecodingError(val value: Json) : DecodingError()
+data class JsStringDecodingError(val value: Json) : DecodingError()
+data class JsBooleanDecodingError(val value: Json) : DecodingError()
+data class JsArrayDecodingError(val value: Json) : DecodingError()
+data class JsObjectDecodingError(val value: Json) : DecodingError()
 data class EnumValueNotFound(val value: Json) : DecodingError()
+data class DateDecodingError(val value: Json, val formatter: Option<DateTimeFormatter>) : DecodingError()
+
+data class ExceptionOnDecoding(
+  val value: Json,
+  val msg: String,
+  val exception: Throwable? = null
+) : DecodingError()
